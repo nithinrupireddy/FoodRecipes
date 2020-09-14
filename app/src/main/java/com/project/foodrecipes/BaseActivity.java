3586a62cd6d2d5ecd867c6a777a9812e7ab2cd6d
Reddy <nithin3116@gmail.com>
2020-09-14 11:17:41 +0530

    Implemented BaseActivity

---
+++ app/src/main/java/com/project/foodrecipes/BaseActivity.java
@@ -0,0 +1,33 @@
+package com.project.foodrecipes;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+
+    public ProgressBar mprogressBar;
+    @Override
+    public void setContentView(int layoutResID) {
+
+        ConstraintLayout constraintLayout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_base,null);
+        FrameLayout frameLayout = constraintLayout.findViewById(R.id.activity_content);
+         mprogressBar = constraintLayout.findViewById(R.id.progressBar);
+
+         mprogressBar.setVisibility(View.GONE);
+         getLayoutInflater().inflate(layoutResID,frameLayout,true);
+
+        super.setContentView(constraintLayout);
+    }
+
+    public void showProgressBar(boolean visible){
+        mprogressBar.setVisibility(visible ? View.VISIBLE : View.INVISIBLE);
+    }
+
+
+}
---
+++ app/src/main/java/com/project/foodrecipes/RecipesList.java
@@ -0,0 +1,40 @@
+package com.project.foodrecipes;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class RecipesList extends BaseActivity {
+
+    private static final String TAG = "Recipes_list";
+
+    //UI Components
+    private Button testButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipes_list);
+
+        initializing_views();
+
+
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mprogressBar.getVisibility() == View.VISIBLE){
+                    Log.d(TAG, "onClick: "+"hide it");
+                    showProgressBar(false);
+                }else{
+                    Log.d(TAG, "onClick: "+"show it");
+                    showProgressBar(true);
+                }
+            }
+        });
+
+    }
+
+    private void initializing_views() {
+        testButton = findViewById(R.id.testButton);
+    }
+}
