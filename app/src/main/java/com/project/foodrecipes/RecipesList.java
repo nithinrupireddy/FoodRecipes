255ee1cd29f5eb9cf8048ad557c94e5d892a963b
Reddy <nithin3116@gmail.com>
2020-10-08 15:42:33 +0530

    integrated retrofit,recyclerview & glide

--- app/src/main/java/com/project/foodrecipes/RecipesList.java
+++ app/src/main/java/com/project/foodrecipes/RecipesList.java
@@ -1,40 +1,85 @@
 package com.project.foodrecipes;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.project.foodrecipes.adapters.RecipeRecyclerAdapter;
+import com.project.foodrecipes.adapters.onRecipeListener;
+import com.project.foodrecipes.models.Recipe;
+import com.project.foodrecipes.viewmodels.RecipeListViewModel;
+
+import java.util.List;
 
 //this is recipesList Activity
-public class RecipesList extends BaseActivity {
+public class RecipesList extends BaseActivity implements onRecipeListener {
 
     private static final String TAG = "Recipes_list";
+    private RecipeListViewModel recipeListViewModel;
+    private Context context = this;
+
+
 
     //UI Components
-    private Button testButton;
+    private RecyclerView recyclerView;
+    private RecipeRecyclerAdapter recipeRecyclerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipes_list);
 
         initializing_views();
+        init_recyclerview();
 
-        testButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(mprogressBar.getVisibility() == View.VISIBLE){
-                    Log.d(TAG, "onClick: "+"hide it");
-                    showProgressBar(false);
-                }else{
-                    Log.d(TAG, "onClick: "+"show it");
-                    showProgressBar(true);
-                }
-            }
-        });
+        recipeListViewModel = new ViewModelProvider(this).get(RecipeListViewModel.class);
+
+        subscribeObservers();
+        test_retrofit_response();
 
     }
 
+    private void init_recyclerview() {
+        recyclerView = findViewById(R.id.recipe_list);
+        recipeRecyclerAdapter = new RecipeRecyclerAdapter(this);
+        recyclerView.setAdapter(recipeRecyclerAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+
+    private void subscribeObservers(){
+      recipeListViewModel.getRecipes().observe(this, new Observer<List<Recipe>>() {
+          @Override
+          public void onChanged(List<Recipe> recipes) {
+              if(recipes!=null){
+                  for(Recipe recipe:recipes){
+                      Log.d(TAG, "getting recipes :"+recipe.getTitle());
+                      recipeRecyclerAdapter.setRecipes(recipes);
+                  }
+              }
+          }
+      });
+    }
+
     private void initializing_views() {
-        testButton = findViewById(R.id.testButton);
+    }
+
+    private void test_retrofit_response(){
+        recipeListViewModel.searchRecipesApi("Chicken",1);
+    }
+
+    @Override
+    public void onRecipeClick(int position) {
+
+    }
+
+    @Override
+    public void onCategoryClick(String category) {
+
     }
 }
